7c93a13e4a001e0c745e54c02c995560b5136940
Eliot Whalan <ewhal@pantsu.cat>
2016-07-05 11:05:09 +1000

    Finish up urlhandler

--- gurl.go
+++ gurl.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"html"
 	"log"
 	"net/http"
 
+	"github.com/dchest/uniuri"
 	"github.com/gorilla/mux"
 )
 
@@ -14,11 +16,16 @@ const (
 	dbNAME     = ""
 	dbPASS     = ""
 	dbUSERNAME = ""
+	LENGTH     = 6
 
 	DATABASE = dbUSERNAME + ":" + dbPASS + "@/" + dbNAME + "?charset=utf8"
 )
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	expiry := r.FormValue("expiry")
+	id := uniuri.NewLen(LENGTH)
+
 }
 
 func delHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,12 +37,18 @@ func delHandler(w http.ResponseWriter, r *http.Request) {
 
 func urlHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	url := vars["urlid"]
+	id := vars["urlid"]
 	db, err := sql.Open("mysql", DATABASE)
 	if err != nil {
 		log.Println(err)
 	}
 	defer db.Close()
+
+	var url, expiry string
+	err = db.QueryRow("select url, expiry from url where id=?", html.EscapeString(id)).Scan(&url, &expiry)
+	if err != nil {
+		log.Println(err)
+	}
 
 	http.Redirect(w, r, url, 303)
 
